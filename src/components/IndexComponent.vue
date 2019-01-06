d67e5abb21a455bf4d3d4d5011755ee270506fc1
Fadli Ramadhan <fadlir93@gmail.com>
2019-01-06 21:37:02 +0700

    Create vue components

---
+++ src/components/IndexComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header"> Index Component </div>
+                <div class="card-body"> I'm an Index Component </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
